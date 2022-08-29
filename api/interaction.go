494367f2b03f41591fdc9abec13c49e48388c7f7
diamondburned <diamond@arikawa-hi.me>
2022-08-28 20:09:45 -0700

    Prevent early closure of SQL connections

--- api/interaction.go
+++ api/interaction.go
@@ -1,8 +1,6 @@
 package handler
 
 import (
-	"context"
-	"log"
 	"net/http"
 
 	"github.com/diamondburned/acmregister-vercel/internal/servutil"
@@ -13,33 +11,47 @@ import (
 	"github.com/pkg/errors"
 )
 
-var interactionHandler = func() http.Handler {
+func HandleInteraction(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
 	botToken, err := env.BotToken()
 	if err != nil {
-		log.Fatalln("cannot get bot token:", err)
+		servutil.WriteErr(w, r, 500, err)
+		return
 	}
 
-	opts, err := env.BotOpts(context.Background())
+	opts, err := env.BotOpts(ctx)
 	if err != nil {
-		log.Fatalln("cannot get bot opts:", err)
+		servutil.WriteErr(w, r, 500, err)
+		return
 	}
 
+	s := state.NewAPIOnlyState(botToken, nil).WithContext(ctx)
+	h := bot.NewHandler(s, opts)
 	serverVars := env.InteractionServer()
 
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		s := state.NewAPIOnlyState(botToken, nil).WithContext(r.Context())
-		h := bot.NewHandler(s, opts)
+	srv, err := webhook.NewInteractionServer(serverVars.PubKey, h)
+	if err != nil {
+		opts.Store.Close()
 
-		srv, err := webhook.NewInteractionServer(serverVars.PubKey, h)
-		if err != nil {
-			servutil.WriteErr(w, r, 500, errors.Wrap(err, "cannot create interaction server"))
-			return
-		}
-		srv.ErrorFunc = servutil.WriteErr
-		srv.ServeHTTP(w, r)
-	})
-}()
+		servutil.WriteErr(w, r, 500, errors.Wrap(err, "cannot create interaction server"))
+		return
+	}
+	srv.ErrorFunc = servutil.WriteErr
+	srv.ServeHTTP(w, r)
 
-func HandleInteraction(w http.ResponseWriter, r *http.Request) {
-	interactionHandler.ServeHTTP(w, r)
+	go func() {
+		// Wait for the interaction to die off before closing. We can't defer
+		// close any of this, because we have to spawn a goroutine for a
+		// follow-up response, so we want to wait until that's done before we
+		// clean up.
+		//
+		// Vercel, of course, behaves in a different way than you would a main()
+		// program, in that any dangling goroutines will actually prevent the
+		// whole serverless function, so this method works. If not for that,
+		// then I don't really know what else to do.
+		h.Wait()
+		h.Close()
+		opts.Store.Close()
+	}()
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/diamondburned/acmregister-vercel
 go 1.18
 
 require (
-	github.com/diamondburned/acmregister v0.0.0-20220829023846-68ebd0d3d963
+	github.com/diamondburned/acmregister v0.0.0-20220829030101-9cb9b56fdd33
 	github.com/diamondburned/arikawa/v3 v3.0.1-0.20220822214349-9e9f90a65248
 	github.com/pkg/errors v0.9.1
 )
